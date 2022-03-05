0cc9ca7d4be7499335bac412c930e144f707ec0b
90150N <mugirethe1@gmail.com>
2022-03-06 00:45:10 +0300

    ft: get select values & check if dates are filled

--- js/app.js
+++ js/app.js
@@ -2,4 +2,12 @@ let btn = document.querySelector("#btn");
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
+  let day = parseInt(document.querySelector("#day").value);
+  let month = parseInt(document.querySelector("#month").value);
+  let year = parseInt(document.querySelector("#year").value);
+  let gender = document.querySelector(".form-check-input:checked").value;
+
+  if (day === 0 || month === 0 || year === 0) {
+    alert("fill all sections");
+  }
 });
