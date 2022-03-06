5018950cb94888ffe51d0d3a59c3f90844077fd8
90150N <mugirethe1@gmail.com>
2022-03-06 20:55:30 +0300

    create name objects

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 let btn = document.querySelector("#btn");
+let maleObj = {
+  sunday: "Kwasi",
+  monday: "Kwadwo",
+  tuesday: "Kwabena",
+  wednesday: "Kwaku",
+  thursday: "Yaw",
+  friday: "Kofi",
+  saturday: "Kwame",
+};
+
+let femaleObj = {
+  sunday: "Akosua",
+  monday: "Adwoa",
+  tuesday: "Abenaa",
+  wednesday: "Akua",
+  thursday: "Yaa",
+  friday: "Afua",
+  saturday: "Ama",
+};
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -8,6 +27,6 @@ btn.addEventListener("click", (e) => {
   let gender = document.querySelector(".form-check-input:checked").value;
 
   if (day === 0 || month === 0 || year === 0) {
-    alert("fill all sections");
+    alert("Kindly fill all sections");
   }
 });
