98634be56662f327a2b72b73054f1a39ad28fe2e
90150N <mugirethe1@gmail.com>
2022-03-07 14:44:10 +0300

    ft: hide form and display name

--- js/app.js
+++ js/app.js
@@ -11,15 +11,17 @@ btn.addEventListener("click", (e) => {
   let month = document.querySelector("#month").value;
   let year = document.querySelector("#year").value;
   let gender = document.querySelector(".form-check-input:checked").value;
+  let formDisplay = document.querySelector(".form-display");
+  let nameDisplay = document.querySelector(".name-display");
 
   const getAkan = (date, month, year) => {
     const userDate = new Date(`${month} ${date} ${year}`);
     let day = userDate.getDay();
 
     if (gender === "male") {
-      nameOut.innerText = `Your Akan Name is ${maleObj[day]}`;
+      nameOut.innerHTML = `Your Akan Name is: <div class= "akan-name d-flex align-items-center justify-content-center"> ${maleObj[day]} </div> `;
     } else {
-      nameOut.innerText = `Your Akan Name is ${femaleObj[day]}`;
+      nameOut.innerHTML = `Your Akan Name is: <div class= "akan-name d-flex align-items-center justify-content-center"> ${femaleObj[day]} </div> `;
     }
   };
 
@@ -27,8 +29,12 @@ btn.addEventListener("click", (e) => {
     btn.classList.toggle("btn-animate");
     if (btn.innerHTML != "Return") {
       btn.innerHTML = "Return";
+      formDisplay.classList.toggle("hide");
+      nameDisplay.classList.toggle("hide");
     } else {
       btn.innerHTML = ` Submit <i class="fa-solid fa-arrow-right">`;
+      formDisplay.classList.toggle("hide");
+      nameDisplay.classList.toggle("hide");
     }
     setTimeout(() => {
       btn.classList.toggle("btn-animate");
